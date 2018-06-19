beb02ef2bb8c4ecfe6eb7e6015bf7d7d49e5231d
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-19 18:01:46 -0400

    Plantillas html y ajustes en django

--- src/caffeteira/urls.py
+++ src/caffeteira/urls.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 
 
 urlpatterns = [
 
-    #urls proyectos
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
-    path('store/', views.store, name="store"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
-    path('sample/', views.sample, name="sample"),
-
+    #path core
+    path('', include('core.urls')),
     #url admin
     path('admin/', admin.site.urls),
 ]
